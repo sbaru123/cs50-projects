64b238e8f1417bf861db825240ce384049d06d1c
sbaru123 <barusuhaan@gmail.com>
2025-07-13 19:50:09 +0000

    Sun, Jul 13, 2025, 3:50 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -13,9 +13,15 @@ void pyramid(int height) {
 
     for (int i = 0; i < height; i++) {
 
-        for (int j = 0; j < 5; j++ ) {
+        for (int spaces = 0; spaces < height - 1; spaces++ ) {
+
+            printf(" ");
+        }
+
+         for (int bricks = height; bricks > 0; spaces++ ) {
 
             printf("#");
+        }
 
     }
 printf("\n");
