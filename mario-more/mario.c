d09ac50ac6b36f173f24a523e427c16342200f95
sbaru123 <barusuhaan@gmail.com>
2025-07-15 17:57:03 +0000

    Tue, Jul 15, 2025, 1:57 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,35 +7,39 @@ int main(void)
 {
     int input = get_int("Height: ");
 
-    while (input > 8 || input < 1) {
+    while (input > 8 || input < 1)
+    {
         input = get_int("Height: ");
     }
 
     pyramid(input);
 }
 
-void pyramid(int height) {
+void pyramid(int height)
+{
 
-    for (int row = 1; row <= height; row++) {
+    for (int row = 1; row <= height; row++)
+    {
 
-        for (int spaces = 0; spaces < height - row; spaces++ ) {
+        for (int spaces = 0; spaces < height - row; spaces++)
+        {
 
             printf(" ");
         }
 
-         for (int bricks = 0; bricks < row; bricks++ ) {
+        for (int bricks = 0; bricks < row; bricks++)
+        {
 
             printf("#");
         }
 
         printf("  ");
 
-         for (int bricks = 0; bricks < row; bricks++ ) {
+        for (int bricks = 0; bricks < row; bricks++)
+        {
 
             printf("#");
         }
         printf("\n");
     }
-
 }
-
