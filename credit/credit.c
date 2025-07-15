ffa75388f8c360204ceca90ec7c3e53098dee881
sbaru123 <barusuhaan@gmail.com>
2025-07-15 18:18:33 +0000

    Tue, Jul 15, 2025, 2:18 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,69 +4,80 @@
 int main(void)
 {
 
-int getLastDigit(long int number);
-int getLastDigitNew(long int number);
-long int card = get_long("Card: ");
-int sum = 0;
-int lastDigit = 0;
-int leftOver = 0;
-long int cardOG = card;
-string name = "";
-int index = 0;
+    //Initialization of all variables
+    int getLastDigit(long int number);
+    int getLastDigitNew(long int number);
+    long int card = get_long("Card: ");
+    int sum = 0;
+    int lastDigit = 0;
+    int leftOver = 0;
+    long int cardOG = card;
+    string name = "";
+    int index = 0;
 
-if (card / 10000000000000 == 34 || card / 10000000000000 == 37) {
-  name = "AMEX";
-  index = 17;
-}
-else if (card / 1000000000000000 == 4 ) {
-  name = "VISA";
-  index = 18;
-}
-else if (card / 1000000000000 == 4) {
-  name = "VISA";
-  index = 15;
-}
-else if (card / 100000000000000 == 51 || card / 100000000000000 == 52 || card / 100000000000000 == 53 || card / 100000000000000 == 54 || card / 100000000000000 == 55) {
-  name = "MASTERCARD";
-  index = 18;
-}
-else {
-  name = "INVALID";
-  index = 20;
-}
-
-//printf("Name: %s\n" , name);
-
-for (int i = 0; i < index; i += 2) {
-
-    lastDigit = getLastDigit(card);
-    card /= 10;
-    lastDigit = lastDigit * 2;
-
-       leftOver = lastDigit % 10;
-       lastDigit = lastDigit / 10;
-
-   // printf("lastDigit: %d\n" , lastDigit);
-  // printf("leftOver: %d\n" , leftOver);
-    sum = sum + lastDigit + leftOver;
-    card /= 10;
+    if (card / 10000000000000 == 34 || card / 10000000000000 == 37)
+    {
+        name = "AMEX";
+        index = 17;
     }
-//printf("Sum: %d\n" , sum);
+    else if (card / 1000000000000000 == 4)
+    {
+        name = "VISA";
+        index = 18;
+    }
+    else if (card / 1000000000000 == 4)
+    {
+        name = "VISA";
+        index = 15;
+    }
+    else if (card / 100000000000000 == 51 || card / 100000000000000 == 52 ||
+             card / 100000000000000 == 53 || card / 100000000000000 == 54 ||
+             card / 100000000000000 == 55)
+    {
+        name = "MASTERCARD";
+        index = 18;
+    }
+    else
+    {
+        name = "INVALID";
+        index = 20;
+    }
 
-for (int j = 0; j < index; j+=2){
-  lastDigit = getLastDigitNew(cardOG);
-  sum = sum + lastDigit;
-  cardOG /= 100;
-}
-//printf("Total Sum: %d\n" , sum);
+    // printf("Name: %s\n" , name);
 
-if (sum % 10 == 0) {
-      printf("%s\n" , name);
-}
-else
-  {
-      printf("INVALID\n");
-  }
+    for (int i = 0; i < index; i += 2)
+    {
+
+        lastDigit = getLastDigit(card);
+        card /= 10;
+        lastDigit = lastDigit * 2;
+
+        leftOver = lastDigit % 10;
+        lastDigit = lastDigit / 10;
+
+        // printf("lastDigit: %d\n" , lastDigit);
+        // printf("leftOver: %d\n" , leftOver);
+        sum = sum + lastDigit + leftOver;
+        card /= 10;
+    }
+    // printf("Sum: %d\n" , sum);
+
+    for (int j = 0; j < index; j += 2)
+    {
+        lastDigit = getLastDigitNew(cardOG);
+        sum = sum + lastDigit;
+        cardOG /= 100;
+    }
+    // printf("Total Sum: %d\n" , sum);
+
+    if (sum % 10 == 0)
+    {
+        printf("%s\n", name);
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 }
 
 int getLastDigit(long int number)
@@ -74,7 +85,7 @@ int getLastDigit(long int number)
     int rem = 0;
     number = number / 10;
     rem = number % 10;
-  // printf("Remainder: %d\n" , rem);
+    // printf("Remainder: %d\n" , rem);
     return rem;
 }
 
@@ -82,6 +93,6 @@ int getLastDigitNew(long int number)
 {
     int rem = 0;
     rem = number % 10;
-   //printf("Remainder: %d\n" , rem);
+    // printf("Remainder: %d\n" , rem);
     return rem;
 }
