fa31e42614df5a6a75f329c9071c554179f621de
sbaru123 <barusuhaan@gmail.com>
2025-08-04 15:02:07 +0000

    Mon, Aug 4, 2025, 11:02 AM -04:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -123,10 +123,17 @@ int main(int argc, string argv[])
     return 0;
 }
 
+//FUNCTIONS
+
+
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+     for (int i = 0; i < candidate_count; i++) {
+        if (strcmp(candidates[i], name) == 0) {
+            
+        }
+    }
     return false;
 }
 
