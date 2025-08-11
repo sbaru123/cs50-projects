78f25e6e64c26b4193034eae18d02e13924d7a65
sbaru123 <barusuhaan@gmail.com>
2025-08-11 19:27:05 +0000

    Mon, Aug 11, 2025, 3:27 PM -04:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -18,17 +18,19 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[512];
     int count = 0;
+    FILE *img = fopen(argv[1] , "w");
 
    while (fread(&buffer, sizeof(uint8_t), 1, memoryCard) == 512) {
     count = 0;
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] && 0xf0) == 0xe0) {
         if (count == 0) {
         sprintf(argv[1] , "%03i.jpg" , count);
+        fwrite(&buffer, sizeof(uint8_t), 1, img)
         count++;
         }
-        else {
-        FILE *img = fopen(argv[1] , "w");
-        fwrite(&buffer, sizeof(uint8_t), 1, img)
+        else (count > 0) {
+            fclose(img);
+
         }
     }
    }
