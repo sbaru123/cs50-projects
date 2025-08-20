33b890bb105f8c80545213e287d49c48d7527ba6
sbaru123 <barusuhaan@gmail.com>
2025-08-20 19:30:44 +0000

    Wed, Aug 20, 2025, 3:30 PM -04:00

--- pset6/readability.py
+++ pset6/readability.py
@@ -3,11 +3,14 @@ from cs50 import get_string
 def main():
     text = get_string("Text: ")
 
-    words = len(text)
+    words = countWords(text)
     letters = countLetters(text)
 
     print(words)
     print(letters)
+
+def countWords(input_string):
+    
 
 def countLetters(input_string):
     letters = [char for char in input_string if char.isalpha()]
