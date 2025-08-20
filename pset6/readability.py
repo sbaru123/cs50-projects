e933f31b8dc5dfbc6f7edcfc3b4b3e50c3ce09ea
sbaru123 <barusuhaan@gmail.com>
2025-08-20 19:43:26 +0000

    Wed, Aug 20, 2025, 3:43 PM -04:00

--- pset6/readability.py
+++ pset6/readability.py
@@ -5,16 +5,26 @@ def main():
 
     words = countWords(text)
     letters = countLetters(text)
+    sentences = countSentences(text)
 
     print(words)
     print(letters)
+
+def countSentences(text):
+    sentCount = 0
+    for char in text:
+        if char == "." or char == "?" or char == "!":
+            sentCount += 1
+
+    return sentCount
+
 
 def countWords(text):
     wordCount = 0
     for char in text:
         if char.isspace() or char == ".":
             wordCount += 1
-            
+
     return wordCount
 
 
