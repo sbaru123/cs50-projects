9aebdfaf087a377f5e308124a962a3d5eb03d575
sbaru123 <barusuhaan@gmail.com>
2025-08-26 14:47:27 +0000

    Tue, Aug 26, 2025, 10:47 AM -04:00

---
+++ pset7/fiftyville/log.sql
@@ -0,0 +1,31 @@
+-- Keep a log of any SQL queries you execute as you solve the mystery.
+
+SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+--10:15 am at bakery robbery intervies with 3 people
+
+SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28 AND year = 2024;
+--Raymond says thief took earliest flight out of fiftyville tomorrow: 7/29/2024. Person bought the ticket around this time
+--Eugune saw thief withdrawing money early in the morning.
+--Ruth within 10 minutes of the theft, thief got into car and drove away
+
+SELECT id, origin_airport_id, destination_airport_id FROM flights WHERE month = 7 AND day = 29 AND year = 2024 ORDER BY hour;
+--Earliest flight id is 36, time is 8:20 am, destination id is 4, LaGuardia Airport, NYC.
+
+
+SELECT abbreviation, full_name, city FROM airports WHERE id = 4;
+--Checking airport
+
+SELECT account_number, atm_location, transaction_type FROM atm_transactions WHERE month = 7 AND day = 28 AND year = 2024 AND transaction_type = 'withdraw' AND atm_location = 'Humphrey Lane';
+--Getting info for atm transaction
+
+
+SELECT name FROM people WHERE id IN
+(SELECT person_id FROM bank_accounts WHERE account_number IN
+(SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND year = 2024 AND transaction_type = 'withdraw' AND atm_location = 'Humphrey Lane'))
+ORDER BY name;
+--Names of all accounts that withdrew money at that day on Humphrey Lane
+
+
+SELECT name FROM people WHERE license_plate IN
+(SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND year = 2024 AND hour = 10 AND minute >= 15 AND minute <= 25)
+ORDER BY name;
---
+++ pset7/songs/7.sql
@@ -0,0 +1,2 @@
+SELECT AVG(energy) FROM songs WHERE songs.artist_id =
+(SELECT id FROM artists WHERE name == 'Drake');
