6ad373a477cc80cb1e0d0be02dfd17cc5b93182f
sbaru123 <barusuhaan@gmail.com>
2025-08-26 15:06:18 +0000

    Tue, Aug 26, 2025, 11:06 AM -04:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -30,4 +30,5 @@ SELECT name FROM people WHERE license_plate IN
 (SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND year = 2024 AND hour = 10 AND minute >= 15 AND minute <= 25 AND activity = 'exit')
 ORDER BY name;
 --Name of people within 10 minutes who were there at the robbery
---Bruce, Iman, and Luca matches both lists where they entered
+--Bruce, Iman, and Luca matches both lists where they left from bakery in this 10 minute time frame 
+
