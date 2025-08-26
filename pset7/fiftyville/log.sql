31ff67beebeaf830d8a71ffc9a96dca2d3d33623
sbaru123 <barusuhaan@gmail.com>
2025-08-26 15:08:37 +0000

    Tue, Aug 26, 2025, 11:08 AM -04:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -7,6 +7,7 @@ SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28 AND year = 
 --Raymond says thief took earliest flight out of fiftyville tomorrow: 7/29/2024. Person bought the ticket around this time
 --Eugune saw thief withdrawing money early in the morning.
 --Ruth within 10 minutes of the theft, thief got into car and drove away
+--As thief was leaveing bakery between 10:15 to 10:25, they called someone for less then a minute
 
 SELECT id, origin_airport_id, destination_airport_id FROM flights WHERE month = 7 AND day = 29 AND year = 2024 ORDER BY hour;
 --Earliest flight id is 36, time is 8:20 am, destination id is 4, LaGuardia Airport, NYC.
@@ -30,5 +31,6 @@ SELECT name FROM people WHERE license_plate IN
 (SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND year = 2024 AND hour = 10 AND minute >= 15 AND minute <= 25 AND activity = 'exit')
 ORDER BY name;
 --Name of people within 10 minutes who were there at the robbery
---Bruce, Iman, and Luca matches both lists where they left from bakery in this 10 minute time frame 
+--Bruce, Iman, and Luca matches both lists where they left from bakery in this 10 minute time frame
+
 
